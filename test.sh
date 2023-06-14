7aa10df9a4242cdcb0acb14660ce39ffdbeec985
chokerman <44473782+justchokingaround@users.noreply.github.com>
2023-06-14 15:33:14 +0200

    refactor: grep + cut is more efficient

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-curl https://anilist.co/sitemap/anime-0.xml | sed -nE "s@.*/anime/([0-9]+)/.*@\1@p" > ids.txt
+curl https://anilist.co/sitemap/anime-0.xml | grep -Eo "anime/([0-9]+)" | cut -d/ -f2 > ids.txt
 
-curl https://anilist.co/sitemap/anime-1.xml | sed -nE "s@.*/anime/([0-9]+)/.*@\1@p" >> ids.txt
+curl https://anilist.co/sitemap/anime-1.xml | grep -Eo "anime/([0-9]+)" | cut -d/ -f2 >> ids.txt
